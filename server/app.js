eeff6f839b7f7f035b93927455e0468ecc1fcabe
Ricky Chon <rickychon99@gmail.com>
2021-10-28 15:53:23 -0400

    Setup routing for compound name and formula

--- server/app.js
+++ server/app.js
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/compound', (req, res) => {
+    res.send('Got a GET request at /compound');
+});
+
+app.get('/compound/name/:compoundName', (req, res) => {
+    res.json(req.params);
+});
+
+app.get('/compound/formula/:formula', (req, res) => {
+    res.json(req.params);
+});
+
 app.listen(port, () => {
     console.log(`OWERT running at http://${hostname}:${port}`);
 });
