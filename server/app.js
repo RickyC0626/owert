c884c5b9f3921eafe9e80af7c293cc094fedfbbb
Ricky Chon <rickychon99@gmail.com>
2021-10-28 17:26:08 -0400

    Fetch compound data from pubchem database

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const fetch = require('node-fetch');
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -19,12 +21,28 @@ app.get('/compound', (req, res) => {
 });
 
 app.get('/compound/name/:compoundName', (req, res) => {
-    res.json(req.params);
+    const compoundName = req.params.compoundName;
+    const compoundURL = `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/${compoundName}/JSON`;
+    getCompound(compoundURL)
+    .then(data => {
+        res.status(200).send(data);
+    }).catch(err => console.error(err));
 });
 
 app.get('/compound/formula/:formula', (req, res) => {
-    res.json(req.params);
+    const formula = req.params.formula;
+    const formulaURL = `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/fastformula/${formula}/JSON`;
+    getCompound(formulaURL)
+    .then(data => {
+        res.status(200).send(data);
+    }).catch(err => console.error(err));
 });
+
+const getCompound = async (url = '') => {
+    // TODO: cache the data if first time fetching, otherwise fetch from cache
+    const response = await fetch(url);
+    return response.json();
+};
 
 app.listen(port, () => {
     console.log(`OWERT running at http://${hostname}:${port}`);
