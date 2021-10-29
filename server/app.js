fd7cfe01cda4fbe33b48a5ff3908957c4f41d9cb
Ricky Chon <rickychon99@gmail.com>
2021-10-29 00:02:03 -0400

    Add in-memory cache for unbaked compound data (#4)

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 const fetch = require('node-fetch');
+const NodeCache = require('node-cache');
+const unbakedDataCache = new NodeCache({ stdTTL: 3000, checkperiod: 1500 });
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -23,23 +25,34 @@ app.get('/compound', (req, res) => {
 app.get('/compound/name/:compoundName', (req, res) => {
     const compoundName = req.params.compoundName;
     const compoundURL = `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/${compoundName}/JSON`;
-    getCompound(compoundURL)
-    .then(data => {
-        res.status(200).send(data);
-    }).catch(err => console.error(err));
+
+    if(unbakedDataCache.has(compoundName))
+        res.status(200).send(unbakedDataCache.get(compoundName));
+    else {
+        getCompound(compoundURL)
+        .then(data => {
+            unbakedDataCache.set(compoundName, data);
+            res.status(200).send(data);
+        }).catch(err => console.error(err));
+    }
 });
 
 app.get('/compound/formula/:formula', (req, res) => {
     const formula = req.params.formula;
     const formulaURL = `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/fastformula/${formula}/JSON`;
-    getCompound(formulaURL)
-    .then(data => {
-        res.status(200).send(data);
-    }).catch(err => console.error(err));
+
+    if(unbakedDataCache.has(formula))
+        res.status(200).send(unbakedDataCache.get(formula));
+    else {
+        getCompound(formulaURL)
+        .then(data => {
+            unbakedDataCache.set(formula, data);
+            res.status(200).send(data);
+        }).catch(err => console.error(err));
+    }
 });
 
 const getCompound = async (url = '') => {
-    // TODO: cache the data if first time fetching, otherwise fetch from cache
     const response = await fetch(url);
     return response.json();
 };
