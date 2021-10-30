fdfdfa9b553c2f552f0b5f0535e55c8db9b887e7
Ricky Chon <rickychon99@gmail.com>
2021-10-30 16:24:38 -0400

    Verify alkyl halide by halogen in compound name

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,24 +3,54 @@ import { useState } from "react";
 function HomePage() {
     const [compoundStatus, setCompoundStatus] = useState('Search for a compound...');
     const [compoundInfo, setCompoundInfo] = useState(null);
+    const [compoundName, setCompoundName] = useState('');
+    const [isAlkylHalide, setAlkylHalide] = useState(false);
+
+    const handleChange = (event) => {
+        setCompoundName(event.target.value);
+    };
 
     const handleNameSubmit = (event) => {
         event.preventDefault();
-        const compoundName = event.target[0].value;
+        const name = event.target[0].value;
 
-        if(compoundName) getCompoundByName(`/compound/name/${compoundName}`);
+        if(name) {
+            setCompoundName(name);
+            getCompoundByName(`/compound/name/${name}`);
+        }
     };
 
     const getCompoundByName = async (url = '') => {
         fetch(url).then(res => {
             if(res.status !== 404) {
                 res.json().then(data => {
-                    setCompoundStatus('Found the compound you were looking for!');
-                    setCompoundInfo(data['PC_Compounds'][0]);
+                    if(data['PC_Compounds']) {
+                        setCompoundStatus(`Compound '${compoundName}' has been found`);
+                        verifyAlkylHalide(compoundName);
+                        setCompoundInfo(data['PC_Compounds'][0]);
+                    }
+                    else {
+                        setCompoundStatus(`There is no valid CID for compound '${compoundName}'`);
+                        setCompoundInfo(null);
+                    }
                 });
             }
-            else setCompoundStatus('Cannot find compound...');
+            else {
+                setCompoundStatus(`Cannot find compound '${compoundName}'`);
+                setCompoundInfo(null);
+            }
         }).catch(err => console.error(err));
+    };
+
+    const verifyAlkylHalide = (name) => {
+        // Compounds that contain halogens (F, Cl, Br, I)
+        // fluoro-, chloro-, bromo-, iodo-
+        const regex = /fluoro|chloro|bromo|iodo/gmi;
+        console.log(name.search(regex));
+        if(name.search(regex) < 0)
+            setAlkylHalide(false);
+        else
+            setAlkylHalide(true);
     };
 
     const Log = ({ value, replacer = null, space = 2 }) => (
@@ -42,12 +72,13 @@ function HomePage() {
 
             <form onSubmit={handleNameSubmit}>
                 <label htmlFor="compoundSearch">Search Compounds By Name:</label><br/>
-                <input type="text" id="compoundSearch" placeholder="methane"/>
+                <input type="text" onChange={handleChange} id="compoundSearch" placeholder="methane"/>
                 <button type="submit" onSubmit={handleNameSubmit}>Search</button>
             </form>
 
             <br/>
             <div>{compoundStatus}</div>
+            <div>{isAlkylHalide ? 'Compound is alkyl halide' : 'Compound is NOT alkyl halide'}</div>
             <Log value={compoundInfo}/>
         </>
     );
